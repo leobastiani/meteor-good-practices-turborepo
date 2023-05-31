12aade83cd3075f5a8b51dcfb9a653ad2a82decc
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2023-05-30 22:43:30 -0300

    add bin/dev

--- apps/web/imports/ui/App.jsx
+++ apps/web/imports/ui/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Header } from 'ui';
 import { Hello } from './Hello.jsx';
 import { Info } from './Info.jsx';
 
 export const App = () => (
   <div>
-    <h1>Welcome to Meteor!</h1>
+    <Header text="Welcome to Meteor!"/>
     <Hello/>
     <Info/>
   </div>
